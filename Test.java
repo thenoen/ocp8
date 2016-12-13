324f242b93ae0dff8dc8df52557ae77376d2be5e
marek <kordikmarek@gmail.com>
2016-12-13 23:01:07 +0100

    added first test, some structure is needed

---
+++ Test.java
@@ -0,0 +1,64 @@
+class Test {
+
+    public static void main (String[] args) {
+        A a = new A();
+        B b = new B();
+        A cast = new B();
+
+        a.echo();
+        b.echo();
+
+        space();
+
+        a.callEcho();
+        b.callEcho();
+
+        space();
+
+        b.callSuperEcho();
+        b.callSuperCallEcho();
+        cast.callEcho();
+        b.callSuperThisEcho();
+    }
+
+    private static void space() {
+        System.out.println("-------------------");
+    }
+
+}
+
+class A {
+    void echo() {
+        System.out.println("a");
+    }
+
+    void callEcho() {
+        echo();
+    }
+
+    void callThisEcho() {
+        this.echo();
+    }
+}
+
+class B extends A {
+    void echo() {
+        System.out.println("b");
+    }
+
+    void callEcho() {
+        echo();
+    }
+
+    void callSuperEcho() {
+        super.echo();
+    }
+
+    void callSuperCallEcho() {
+        super.callEcho();
+    }
+
+    void callSuperThisEcho() {
+        super.callThisEcho();
+    }
+}
