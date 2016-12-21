3a6ba9ffb7de44ce4f6f753eed04929c873987b5
marek <kordikmarek@gmail.com>
2016-12-21 20:34:29 +0100

    added structure; added samples for multiple classes in one file on the same level

---
+++ chapter_1/many_classes_one_file/MultiClass.java
@@ -0,0 +1,17 @@
+public class MultiClass {
+
+}
+
+// only "package-private" access allowed
+class A {
+
+}
+
+// not allowed: "public class" has to be specified in a file with the same name
+// public class B {}
+
+// not allowed: modifier is not allowed here
+// protected class C {}
+
+// not allowed: modifier is not allowed here
+// private class D {}
