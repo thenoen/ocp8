ce6ecd9eea657709fa468605e81e7a65e1b8e17e
marek <kordikmarek@gmail.com>
2016-12-21 20:57:04 +0100

    added .gitignore file

---
+++ chapter_1/nested_classes/MemberInnerClass.java
@@ -0,0 +1,14 @@
+public class MemberInnerClass {
+
+    // This is "member inner class"
+    class A {}
+
+    static class B {}
+
+    private class C {}
+
+    protected class D {}
+
+    public class E{}
+
+}
