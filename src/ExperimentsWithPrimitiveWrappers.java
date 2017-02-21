685e52428e56f9a5be1ad41f7b0a43b4dda8ed9e
marek <kordikmarek@gmail.com>
2017-02-21 21:19:30 +0100

    added experiments with primitives and primitive wrappers

---
+++ src/ExperimentsWithPrimitiveWrappers.java
@@ -0,0 +1,18 @@
+
+public class ExperimentsWithPrimitiveWrappers {
+
+	public static void main(String[] args) {
+		Double da = 0x1; da = new Integer(1);
+		Double db = 1;
+		Integer ic = 1f;
+
+		Integer i = 1L;
+		Integer ii = 1;
+		Short j = 1; j = ii;
+		Character k = 1; k = j;
+		Byte b = 1; b = k;
+
+		da = ic;
+	}
+
+}
---
+++ src/ExperimentsWithPrimitives.java
@@ -0,0 +1,18 @@
+
+public class ExperimentsWithPrimitives {
+
+	public static void main(String[] args) {
+		double da = 0x1;
+		double db = 1;
+		int ic = 1f;
+
+		int i = 1L;
+		int ii = 1;
+		short j = 1; j = ii;
+		char k = 1; k = j;
+		byte b = 1; b = k;
+
+		da = ic;
+	}
+
+}
